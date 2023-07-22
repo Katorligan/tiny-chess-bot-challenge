8a18e9d3b79d063edbec319c6ba6a68b83d845ac
Katorligan <katorligan@gmail.com>
2023-07-22 16:39:29 +0200

    feat: bot plays mate in 1, else plays random move

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,31 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move[] legalMoves = board.GetLegalMoves();
+
+        foreach (Move move in legalMoves)
+        {
+            if (MoveIsCheckmate(board, move))
+            {
+                return move;
+            }
+        }
+
+        Random rng = new();
+
+        return legalMoves[rng.Next(legalMoves.Length)];
+    }
+
+    bool MoveIsCheckmate(Board board, Move move)
+    {
+        board.MakeMove(move);
+        bool isCheckmate = board.IsInCheckmate();
+        board.UndoMove(move);
+
+        return isCheckmate;
     }
 }
